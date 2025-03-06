d80170e2f7c26ab831c0dce3fbefbef394bd985b
zengji12 <rezaardiansyahyudhanegara@gmail.com>
2025-03-06 11:02:09 +0700

    add route to '/', update password colomn and hashing password, changes petugas to pembuat on FormKTPResponse

--- models.py
+++ models.py
@@ -8,13 +8,13 @@ class Pengguna(db.Model):
     __tablename__ = 'pengguna'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(255), nullable=False)#jadi 255
     nama_lengkap = db.Column(db.String(100), nullable=False)
     jabatan = db.Column(db.String(50))
     role = db.Column(db.String(50), nullable=False)  # 'admin' atau 'user'
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password = generate_password_hash(password, method='scrypt')#nambah method
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
